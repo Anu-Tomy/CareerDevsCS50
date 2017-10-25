9f1dd5e88d236e3daa99f8d484fbddb1ad72be47
Anu Tomy <anuatomy@gmail.com>
2017-10-25 15:38:24 +0000

    Add number validation

--- Pset1/water/water.c
+++ Pset1/water/water.c
@@ -5,7 +5,12 @@ int main(void)
 {
     //Input
     printf("Minutes: ");
-    int minutes = get_int();
+    int minutes;
+    do
+    {
+        minutes = get_int();
+    }
+    while (minutes < 0);
     //Output
     printf("Bottles: %i\n", (minutes * 12));
 }
